eb48176397b68c4c58f3716e9923bdae2232cb13
Vicamo Yang (楊有勝) <vicamo@gmail.com>
2014-02-20 21:27:34 +0800

    add libchewing

---
+++ Library/Formula/libchewing.rb
@@ -0,0 +1,18 @@
+require 'formula'
+
+class Libchewing < Formula
+  homepage 'http://chewing.im/'
+  url 'https://github.com/chewing/libchewing/releases/download/v0.3.5/libchewing-0.3.5.tar.bz2'
+  sha1 '5ee3941f0f62fa14fbda53e1032970b04a7a88b7'
+
+  head 'https://github.com/chewing/libchewing.git'
+
+  depends_on 'cmake' => :build
+
+  def install
+    mkdir 'libchewing-build' do
+      system "cmake", "..", *std_cmake_args
+      system "make install"
+    end
+  end
+end
