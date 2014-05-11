86f25ed08d9a3e55a5b8d727ab10004f0beaa336
Vicamo Yang <vicamo@gmail.com>
2014-05-11 21:37:21 +0800

    [DO NOT SUBMIT] for local test only

--- Library/Formula/libchewing.rb
+++ Library/Formula/libchewing.rb
@@ -2,10 +2,7 @@ require 'formula'
 
 class Libchewing < Formula
   homepage 'http://chewing.im/'
-  url 'https://github.com/chewing/libchewing/releases/download/v0.3.5/libchewing-0.3.5.tar.bz2'
-  sha1 '5ee3941f0f62fa14fbda53e1032970b04a7a88b7'
-
-  head 'https://github.com/chewing/libchewing.git'
+  head 'https://github.com/vicamo/libchewing.git'
 
   depends_on 'cmake' => :build
   depends_on 'texinfo' => :build
